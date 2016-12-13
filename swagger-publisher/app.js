c82799b594cb7bf528863c5e317b12042a4ce6b1
Santiago Pericas-Geertsen <Santiago.PericasGeertsen@oracle.com>
2016-12-13 15:09:09 -0500

    File upload logic.

--- swagger-publisher/app.js
+++ swagger-publisher/app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var config = require('./routes/config');
@@ -19,11 +17,9 @@ app.set('view engine', 'jade');
 // Uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Setup routes
 app.use('/', index);
 app.use('/config', config);
 app.use('/publish', publish);
--- swagger-publisher/routes/publish.js
+++ swagger-publisher/routes/publish.js
@@ -1,21 +1,44 @@
 
 var express = require('express');
+var multer = require('multer');
+var fs = require('fs');
+
+var upload = multer({ dest: 'public/uploads' });
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
     res.send("Use POST to upload a Swagger file")
 });
 
-router.post('/', function(req, res) {
-    var contentType = req.headers['content-type'];
+router.post('/', upload.single('swaggerFile'), function(req, res) {
+    if (!req.file) {
+        res.status(400).send("No files uploaded");
+        return;
+    }
+
+    var swaggerFile = req.file.path;
+    var contentType = req.file.mimetype;
     if (contentType.includes("html")) {
-        res.send("Attempting to upload Swagger file in HTML")
-    } else if (contentType.includes("json")) {
-        res.send("Attempting to upload Swagger file in JSON")
-    } else if (contentType.includes("yaml")) {
-        res.send("Attempting to upload Swagger file in YAML")
-    } else {
-        // Error
+        console.log("Uploading file as HTML " + JSON.stringify(req.file));
+
+        // Add HTML extension to file
+        fs.rename(swaggerFile, swaggerFile + ".html", function(err) {
+            if (err) {
+                res.status(500).send("Unable to rename file " + swaggerFile);
+            }
+        });
+
+        // Redirect to uploaded file
+        res.redirect('uploads/' + req.file.filename + ".html");
+    }
+    else if (contentType.includes("json")) {
+        console.log("Uploading file as JSON " + JSON.stringify(req.file));
+    }
+    else if (contentType.includes("yaml")) {
+        console.log("Uploading file as YAML " + JSON.stringify(req.file));
+    }
+    else {
+        res.status(400).send("Invalid content type");
     }
 });
 
